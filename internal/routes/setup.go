b2bd3b3659ae7a1b770bdbb018aef4f378a70fb6
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-01-13 19:49:31 +0100

    setup users routes

--- internal/routes/setup.go
+++ internal/routes/setup.go
@@ -1,5 +1,11 @@
 package routes
 
-import "github.com/Adedunmol/wish-mate/internal/config"
+import (
+	"github.com/Adedunmol/wish-mate/internal/config"
+	"github.com/Adedunmol/wish-mate/internal/user"
+)
 
-func SetupRoutes(config config.Config) {}
+func SetupRoutes(config config.Config) {
+
+	user.UserRoutes(config)
+}
--- internal/user/routes.go
+++ internal/user/routes.go
@@ -2,6 +2,20 @@ package user
 
 import (
 	"github.com/Adedunmol/wish-mate/internal/config"
+	"github.com/go-chi/chi/v5"
+	"net/http"
 )
 
-func UserRoutes(config config.Config) {}
+func UserRoutes(config config.Config) {
+
+	userRouter := chi.NewRouter()
+
+	store := NewUserStore(config.DB)
+
+	handler := Handler{Store: store}
+
+	userRouter.Patch("/register", http.HandlerFunc(handler.CreateUserHandler))
+	userRouter.Post("/login", http.HandlerFunc(handler.LoginUserHandler))
+
+	config.Router.Mount("/users", userRouter)
+}
