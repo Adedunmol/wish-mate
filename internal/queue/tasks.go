7f1ea050c8f419b7789a11e39d5ae1aaaca07627
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-01-20 14:38:24 +0100

    add factory for creating tasks

--- internal/queue/tasks.go
+++ internal/queue/tasks.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"context"
+	"fmt"
 	"github.com/hibiken/asynq"
 )
 
@@ -17,4 +18,33 @@ type TaskPayload struct {
 
 type Queue interface {
 	Enqueue(taskPayload *TaskPayload) error
+}
+
+type Client struct {
+	client *asynq.Client
+}
+
+func (qc *Client) Enqueue(taskPayload *TaskPayload) error {
+
+	switch taskPayload.Type {
+	case TypeEmailDelivery:
+		emailPayload := EmailDeliveryPayload{
+			Email:    taskPayload.Payload["email"].(string),
+			Template: taskPayload.Payload["template"].(string),
+			Subject:  taskPayload.Payload["subject"].(string),
+			Data:     map[string]interface{}{},
+		}
+
+		task, err := emailPayload.NewTask()
+		if err != nil {
+			return fmt.Errorf("error creating new email task: %v", err)
+		}
+
+		_, err = qc.client.Enqueue(task)
+		if err != nil {
+			return fmt.Errorf("could not enqueue mail task for: %s: %v", emailPayload.Email, err)
+		}
+	}
+
+	return nil
 }
