f7f51255be2a8d99863616fbee7ebbb295adc659
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-01-24 10:02:45 +0100

    add store and associated methods

--- internal/wishlist/store.go
+++ internal/wishlist/store.go
@@ -1,8 +1,37 @@
 package wishlist
 
+import "database/sql"
+
 type Store interface {
 	CreateWishlist(userID int, body Wishlist) (WishlistResponse, error)
 	GetWishlistByID(wishlistID, userID int) (WishlistResponse, error)
 	UpdateWishlistByID(wishlistID, userID int, body UpdateWishlist) (WishlistResponse, error)
 	DeleteWishlistByID(wishlistID, userID int) error
+}
+
+type WishlistStore struct {
+	db *sql.DB
+}
+
+func NewWishlistStore(db *sql.DB) *WishlistStore {
+
+	return &WishlistStore{db: db}
+}
+
+func (w *WishlistStore) CreateWishlist(userID int, body Wishlist) (WishlistResponse, error) {
+
+	return WishlistResponse{}, nil
+}
+
+func (w *WishlistStore) GetWishlistByID(wishlistID, userID int) (WishlistResponse, error) {
+	return WishlistResponse{}, nil
+}
+
+func (w *WishlistStore) UpdateWishlistByID(wishlistID, userID int, body UpdateWishlist) (WishlistResponse, error) {
+
+	return WishlistResponse{}, nil
+}
+
+func (w *WishlistStore) DeleteWishlistByID(wishlistID, userID int) error {
+	return nil
 }
