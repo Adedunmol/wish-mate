41c37a5f161c7cff491cb3effe9b8581a1f3de34
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-01-25 12:41:22 +0100

    add tests for friends routes

---
+++ internal/friend/friend_test.go
@@ -0,0 +1,46 @@
+package friend_test
+
+import "testing"
+
+func TestSendRequest(t *testing.T) {
+
+	t.Run("send a request and return the entry", func(t *testing.T) {})
+
+	t.Run("return 404 for no user with the id", func(t *testing.T) {})
+
+	t.Run("return bad request for empty user id", func(t *testing.T) {})
+}
+
+func TestAcceptRequest(t *testing.T) {
+
+	t.Run("accept a request and return the entry", func(t *testing.T) {})
+
+	t.Run("return 404 for no entry with the request id", func(t *testing.T) {})
+
+	t.Run("return bad request for empty request id", func(t *testing.T) {})
+}
+
+func TestGetAllFriendships(t *testing.T) {
+
+	t.Run("return all friendships (pending)", func(t *testing.T) {})
+
+	t.Run("return all friendships (accepted)", func(t *testing.T) {})
+
+	t.Run("return all friendships (blocked)", func(t *testing.T) {})
+}
+
+func TestGetFriendship(t *testing.T) {
+
+	t.Run("return a friendship", func(t *testing.T) {})
+}
+
+func UpdateFriendship(t *testing.T) {
+
+	t.Run("update and return a friendship", func(t *testing.T) {})
+
+	t.Run("return forbidden", func(t *testing.T) {})
+
+	t.Run("invalid request body", func(t *testing.T) {})
+
+	t.Run("return not found for friendship id not found", func(t *testing.T) {})
+}
