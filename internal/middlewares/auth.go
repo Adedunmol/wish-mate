683cf4744797210d457da41947d9b121a212045d
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-01-29 16:51:10 +0100

    add middleware to validate and decode jwt tokens

---
+++ internal/middlewares/auth.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"context"
+	"github.com/Adedunmol/wish-mate/internal/helpers"
+	"net/http"
+	"strings"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
+
+		authHeader := request.Header.Get("Authorization")
+		if authHeader == "" {
+			helpers.HandleError(responseWriter, helpers.ErrUnauthorized)
+			return
+		}
+
+		tokenString := strings.Split(authHeader, " ")[1]
+		if len(tokenString) != 2 {
+			helpers.HandleError(responseWriter, helpers.ErrUnauthorized)
+			return
+		}
+
+		data, err := helpers.DecodeToken(tokenString)
+		if err != nil {
+			helpers.HandleError(responseWriter, helpers.ErrUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(request.Context(), "email", data["email"])
+		ctx = context.WithValue(ctx, "user_id", data["user_id"])
+
+		newRequest := request.WithContext(ctx)
+		next.ServeHTTP(responseWriter, newRequest)
+	})
+}
