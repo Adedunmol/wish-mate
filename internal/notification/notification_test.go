0fc7c0d170a29436aaa627d0b6d8a63e483d61ee
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-02-01 14:47:11 +0100

    add tests for notification

--- internal/notification/notification_test.go
+++ internal/notification/notification_test.go
@@ -1,1 +1,13 @@
 package notification_test
+
+import "testing"
+
+func TestCreateNotification(t *testing.T) {}
+
+func TestGetNotification(t *testing.T) {}
+
+func TestGetUserNotifications(t *testing.T) {}
+
+func TestUpdateNotification(t *testing.T) {}
+
+func TestDeleteNotification(t *testing.T) {}
--- internal/notification/store.go
+++ internal/notification/store.go
@@ -3,5 +3,7 @@ package notification
 type Store interface {
 	CreateNotification(body *CreateNotificationBody) (Notification, error)
 	UpdateNotification(ID int, status string) (Notification, error)
+	GetNotification(ID int) (Notification, error)
+	GetUserNotifications(userID int) ([]Notification, error)
 	DeleteNotification(ID int) error
 }
