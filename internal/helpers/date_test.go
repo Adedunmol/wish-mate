70224af20df8b1811828ed9c84027f63fa1fe340
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-02-05 21:44:18 +0100

    add test file for calculating the days before a date

---
+++ internal/helpers/date_test.go
@@ -0,0 +1,10 @@
+package helpers_test
+
+import "testing"
+
+func TestCalculateDaysBefore(t *testing.T) {
+
+	t.Run("calculate the exact date before a given date", func(t *testing.T) {})
+
+	t.Run("return error for invalid date", func(t *testing.T) {})
+}
