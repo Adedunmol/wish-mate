b7ec9e3cc0961dad1a25455ba0bbc2e6efd57378
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-02-07 18:47:31 +0100

    remove redundant handler for getting friends

--- internal/user/user.go
+++ internal/user/user.go
@@ -228,6 +228,4 @@ func (h *Handler) GetRequestHandler(responseWriter http.ResponseWriter, request 
 	helpers.WriteJSONResponse(responseWriter, response, http.StatusOK)
 }
 
-func (h *Handler) GetAllFriends(responseWriter http.ResponseWriter, request *http.Request) {}
-
 func (h *Handler) GetUser(responseWriter http.ResponseWriter, request *http.Request) {}
