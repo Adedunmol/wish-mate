48e8bb0df74dd0dc94131099e746b4cb938fb575
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-02-24 05:14:15 +0100

    refactor jobs to being scheduled using cron instead of Golang's time ticker.

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/Adedunmol/wish-mate/internal/config"
 	"github.com/Adedunmol/wish-mate/internal/queue"
 	"github.com/Adedunmol/wish-mate/internal/reminder"
 	"github.com/Adedunmol/wish-mate/internal/routes"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-co-op/gocron/v2"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -40,15 +43,59 @@ func main() {
 		log.Fatal(errors.Unwrap(err))
 	}
 
+	// create a new scheduler
+	scheduler, err := gocron.NewScheduler()
+	if err != nil {
+		log.Fatal(fmt.Errorf("error starting gocron scheduler: %v", errors.Unwrap(err)))
+	}
+
+	// schedule a task to run every minute
+	_, err = scheduler.NewJob(
+		gocron.DurationJob(time.Minute),
+		gocron.NewTask(func() {
+			enqueueReminders(qc, db)
+		}),
+	)
+	if err != nil {
+		log.Fatalf("failed to schedule job: %v", errors.Unwrap(err))
+	}
+
+	go scheduler.Start()
+
 	r := chi.NewRouter()
 
 	routes.SetupRoutes(config.Config{DB: db, Router: r, Queue: qc})
 
-	go checkScheduledJobs(qc, db)
-
 	go qc.Run(ctx, db)
 
-	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), r))
+	// handle graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	server := &http.Server{Addr: fmt.Sprintf(":%d", os.Getenv("PORT")), Handler: r}
+	go func() {
+		log.Printf("starting web server on port %d", os.Getenv("PORT"))
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("error starting web server on port %d: %v", os.Getenv("PORT"), err)
+		}
+	}()
+
+	<-stop
+
+	err = scheduler.Shutdown()
+	if err != nil {
+		log.Fatal(fmt.Errorf("error shutting down scheduler: %v", errors.Unwrap(err)))
+	}
+
+	// gracefully shutdown the server
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("server forced to shut down: %v", err)
+	}
+
+	log.Println("server exited properly")
 }
 
 func handlePanics() {
@@ -57,20 +104,16 @@ func handlePanics() {
 	}
 }
 
-func checkScheduledJobs(client *queue.Client, db *pgx.Conn) {
-
+func enqueueReminders(client *queue.Client, db *pgx.Conn) {
+	currentTime := time.Now()
 	taskStore := &reminder.ReminderStore{DB: db}
-	ticker := time.NewTicker(time.Minute)
-	defer ticker.Stop()
 
-	for t := range ticker.C {
-		// check db for reminders where scheduled = pending AND scheduled_at <= now
-		log.Printf("checking due scheduled reminders at: %v", t.UTC())
+	// check db for reminders where scheduled = pending AND scheduled_at <= now
+	log.Printf("checking due scheduled reminders at: %v", currentTime.UTC())
 
-		if err := reminder.EnqueueReminders(taskStore, client, &t); err != nil {
-			log.Printf(errors.Unwrap(err).Error())
-		}
-
-		// get today's birthdays and send notifications to their friends
+	if err := reminder.EnqueueReminders(taskStore, client, &currentTime); err != nil {
+		log.Printf(errors.Unwrap(err).Error())
 	}
+
+	// get today's birthdays and send notifications to their friends
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/go-playground/validator/v10 v10.23.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/hibiken/asynq v0.25.1
+	github.com/jackc/pgx/v5 v5.7.2
+	github.com/joho/godotenv v1.5.1
 	github.com/redis/go-redis/v9 v9.7.0
 	golang.org/x/crypto v0.32.0
 )
@@ -17,16 +19,18 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-co-op/gocron/v2 v2.15.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/spf13/cast v1.7.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
