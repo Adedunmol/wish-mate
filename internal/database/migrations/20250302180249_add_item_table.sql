10426cb6d19d21e49960b765d387558cbdb5d410
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-03-02 19:06:51 +0100

    add items table (not migrated yet)

---
+++ internal/database/migrations/20250302180249_add_item_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    link VARCHAR(255),
+    taken BOOLEAN default false,
+    created_by SERIAL references users(id),
+    wishlist_id SERIAL references wishlists(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE items;
+-- +goose StatementEnd
