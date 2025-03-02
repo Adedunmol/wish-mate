7124ec1f501a084b6c89866545461608993b841d
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-03-02 19:24:13 +0100

    add friendships table (not migrated yet)

---
+++ internal/database/migrations/20250302180914_add_friendship_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TYPE status AS ENUM ('pending', 'accepted', 'blocked')
+
+CREATE TABLE friendships (
+    status status NOT NULL default 'pending',
+    user_id INTEGER NOT NULL references users(id),
+    friend_id INTEGER NOT NULL references users(id),
+    PRIMARY KEY (user_id, friend_id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE friendships;
+DROP TYPE status;
+-- +goose StatementEnd
