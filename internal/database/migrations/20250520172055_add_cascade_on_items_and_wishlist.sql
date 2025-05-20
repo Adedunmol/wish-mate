0eb9544b7cd350b1fa72a77cef9a812615a270f2
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-05-20 18:34:27 +0100

    add migration for cascaded deletions

---
+++ internal/database/migrations/20250520172055_add_cascade_on_items_and_wishlist.sql
@@ -0,0 +1,29 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE items DROP CONSTRAINT items_wishlist_id_fkey;
+
+ALTER TABLE items
+ADD CONSTRAINT items_wishlist_id_fkey
+FOREIGN KEY (wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE;
+
+ALTER TABLE items DROP CONSTRAINT items_wishlist_id_fkey;
+
+ALTER TABLE item_picks
+ADD CONSTRAINT item_picks_item_id_fkey
+FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE items DROP CONSTRAINT items_wishlist_id_fkey;
+
+ALTER TABLE items
+ADD CONSTRAINT items_wishlist_id_fkey
+FOREIGN KEY (wishlist_id) REFERENCES wishlists(id);  -- without cascade
+
+ALTER TABLE item_picks DROP CONSTRAINT item_picks_item_id_fkey;
+
+ALTER TABLE item_picks
+ADD CONSTRAINT item_picks_item_id_fkey
+FOREIGN KEY (item_id) REFERENCES items(id);  -- without cascade
+-- +goose StatementEnd
