b846790af9e1fc7e57e961ff4df7a7f13a603000
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-05-21 16:59:50 +0100

    fix error in migration script

--- internal/database/migrations/20250520172055_add_cascade_on_items_and_wishlist.sql
+++ internal/database/migrations/20250520172055_add_cascade_on_items_and_wishlist.sql
@@ -1,29 +1,29 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE items DROP CONSTRAINT items_wishlist_id_fkey;
+ALTER TABLE items DROP CONSTRAINT IF EXISTS items_wishlist_id_fkey;
 
 ALTER TABLE items
-ADD CONSTRAINT items_wishlist_id_fkey
-FOREIGN KEY (wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE;
+    ADD CONSTRAINT items_wishlist_id_fkey
+        FOREIGN KEY (wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE;
 
-ALTER TABLE items DROP CONSTRAINT items_wishlist_id_fkey;
+ALTER TABLE item_picks DROP CONSTRAINT IF EXISTS item_picks_item_id_fkey;
 
 ALTER TABLE item_picks
-ADD CONSTRAINT item_picks_item_id_fkey
-FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE;
+    ADD CONSTRAINT item_picks_item_id_fkey
+        FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE items DROP CONSTRAINT items_wishlist_id_fkey;
+ALTER TABLE items DROP CONSTRAINT IF EXISTS items_wishlist_id_fkey;
 
 ALTER TABLE items
-ADD CONSTRAINT items_wishlist_id_fkey
-FOREIGN KEY (wishlist_id) REFERENCES wishlists(id);  -- without cascade
+    ADD CONSTRAINT items_wishlist_id_fkey
+        FOREIGN KEY (wishlist_id) REFERENCES wishlists(id);  -- without cascade
 
-ALTER TABLE item_picks DROP CONSTRAINT item_picks_item_id_fkey;
+ALTER TABLE item_picks DROP CONSTRAINT IF EXISTS item_picks_item_id_fkey;
 
 ALTER TABLE item_picks
-ADD CONSTRAINT item_picks_item_id_fkey
-FOREIGN KEY (item_id) REFERENCES items(id);  -- without cascade
+    ADD CONSTRAINT item_picks_item_id_fkey
+        FOREIGN KEY (item_id) REFERENCES items(id);  -- without cascade
 -- +goose StatementEnd
